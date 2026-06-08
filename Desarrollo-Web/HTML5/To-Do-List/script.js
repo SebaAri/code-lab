b68137d678cf409f7dfe0b863f6c1ea5c4a6939e
Ariel <155785268+SebaAri@users.noreply.github.com>
2026-06-07 23:22:41 -0300

    Agrego funciones

--- Desarrollo-Web/HTML5/To-Do-List/script.js
+++ Desarrollo-Web/HTML5/To-Do-List/script.js
@@ -4,48 +4,76 @@ function cambiarColor() {
   const backcolor = "rgb(0, 252, 0)";
   btn.style.backgroundColor = backcolor;
 }
-
 function mostrarTexto() {
-  const input = document.getElementById("fname");
-  const resultado = document.getElementById("resultado");
 
+  const input = document.getElementById("fname");
+    if (input.value.trim() === "") {
+  return;
+}
+  const resultado = document.getElementById("resultado");
   const li = document.createElement("li");
 
   li.innerHTML = `
     <span>${input.value}</span>
     <button class="editar">Editar</button>
     <button class="eliminar">Eliminar</button>
+    <button class="guardar">Guardar</button>
+    
   `;
 
   resultado.appendChild(li);
 
-  input.value = "";
+  const guardarBtn = li.querySelector(".guardar");
+  guardarBtn.addEventListener("click", () => {
+    const tareaTexto = li.querySelector("span").textContent;
+    const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+
+    if (!tareas.includes(tareaTexto)) {
+      tareas.push(tareaTexto);
+      localStorage.setItem("tareas", JSON.stringify(tareas));
+      alert("Tarea guardada en localStorage");
+      mostrarTareasGuardadas();
+    }
+
+});
+
+
 
   const editarBtn = li.querySelector(".editar");
-
   editarBtn.addEventListener("click", () => {
     const tarea = li.querySelector("span");
-
     const nuevoTexto = prompt("Ingrese el nuevo texto de la tarea:");
-
     if (nuevoTexto) {
       tarea.textContent = nuevoTexto;
     }
   });
+
+  const eliminarBtn = li.querySelector(".eliminar");
+  eliminarBtn.addEventListener("click", () => {
+    const Texto = prompt("Desea eliminar esta tarea? si/no");
+    if (Texto === "si") {
+      li.remove();
+    }
+  });
+
+  input.value = "";
+}
+function mostrarTareasGuardadas() {
+    const listaGuardadas = document.getElementById("lista-guardadas");
+
+    listaGuardadas.innerHTML = "";
+
+    const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+
+    tareas.forEach(tarea => {
+        const li = document.createElement("li");
+        li.textContent = tarea;
+        listaGuardadas.appendChild(li);
+    });
 }
 
-function eliminarTarea() {
-  const eliminarBtn = document.querySelectorAll(".eliminar");
-  eliminarBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const Texto = prompt("Desea eliminar esta tarea? si/no");
-      if (Texto === "si") {
-        btn.parentElement.remove();
-      }
-    });
-  });
-}
+
 
 btn.addEventListener("click", cambiarColor);
 btn.addEventListener("click", mostrarTexto);
-btn.addEventListener("click", eliminarTarea);
+
